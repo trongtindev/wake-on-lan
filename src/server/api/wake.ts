de30128b2d8196794308de45c3242353e7ef5a17
Tin Nguyen Trong <me@trongtin.dev>
2023-11-01 10:25:03 +0700

    feat: added commitlint

--- src/server/api/wake.ts
+++ src/server/api/wake.ts
@@ -1,9 +1,7 @@
 import admin from 'firebase-admin';
 
 export default defineEventHandler(async (event) => {
-	const serviceAccount = JSON.parse(
-		process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
-	);
+	const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
 	if (!admin.apps || admin.apps.length === 0) {
 		admin.initializeApp({
 			credential: admin.credential.cert(serviceAccount),
@@ -14,7 +12,7 @@ export default defineEventHandler(async (event) => {
 	const token = event.node.req.headers?.authorization?.split(' ')[1];
 	const decodedToken = await admin.auth().verifyIdToken(token ?? '');
 
-	const body: { mac: string, ipv4: string; } = await readBody(event);
+	const body: { mac: string; ipv4: string } = await readBody(event);
 	const result = await admin.messaging().sendToTopic(`wol.${decodedToken.uid}`, {
 		data: body
 	});
